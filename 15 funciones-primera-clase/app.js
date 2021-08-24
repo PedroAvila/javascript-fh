8815bf1a144ff8eef380cc97cc6a3998e1a5a8af
PedroAvila <minimainor_austin@hotmail.com>
2021-08-24 11:23:18 -0500

    funciones de primera clase

---
+++ 15 funciones-primera-clase/app.js
@@ -0,0 +1,20 @@
+
+function a(){
+    console.log( "Función a" );
+}
+
+a();
+
+a.nombre = "María";
+a.direccion  = {
+    pais: "Perú",
+    ciudad: "Lima",
+    edificio: {
+        piso: "3ro",
+        nombre: "Familia Núñez"
+    }
+}
+
+
+
+
